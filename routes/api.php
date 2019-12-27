9259d15fc672d71ab61c294d4255a5303ab63369
CapooCat <54877892+CapooCat@users.noreply.github.com>
2019-12-27 14:58:35 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::get('tap-tho/{id}', 'API\BaiThoController@layBaiTho');
 Route::get('linh-vuc','API\LinhVucController@layDanhSach');
 Route::post('register','API\Auth\RegisterController@register');
 Route::post('dang-nhap','API\DangNhapController@dangNhap');
+Route::post('credit','API\GoiCreditController@layDanhSach');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
