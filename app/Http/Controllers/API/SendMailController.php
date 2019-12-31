d0c2ca8f72871cff22b454d308fa232f89b254a6
nhoxking500 <huyduc2741999@gmail.com>
2019-12-31 14:09:42 +0700

    update

--- app/Http/Controllers/API/SendMailController.php
+++ app/Http/Controllers/API/SendMailController.php
@@ -15,30 +15,41 @@ class SendMailController extends Controller
     {
     	//Nhận post tên đăng nhập
     	$user_id=NguoiChoi::where('ten_dang_nhap','=',$request->ten_dang_nhap)->first();
-    	$user=NguoiChoi::find($user_id->id);
-    	if($user=="" && $user_id=="")
-    	{
-    		$res = [
+        if($user_id=="")
+        {
+            $res = [
                 'success'   => false,
-                'msg'       => 'Tìm mật khẩu thất bại'
+                'msg'       => 'Tài khoản ko tồn tại'
             ];
             return \response()->json($res);
-    	}
-    	else
-    	{
-    		 $user->mat_khau=Str::random(6);
-    		 $user->save();
-    		 //Chọn gmail muốn gửi
-    		 Mail::to($user->email)->send(new MailQuenMatKhau($user));
+        }
+        else
+        {
+        	$user=NguoiChoi::find($user_id->id);
+            if($user=="")
+        	{
+        		$res = [
+                    'success'   => false,
+                    'msg'       => 'Tìm mật khẩu thất bại'
+                ];
+                return \response()->json($res);
+        	}
+        	else
+        	{
+        		 $user->mat_khau=Str::random(6);
+        		 $user->save();
+        		 //Chọn gmail muốn gửi
+        		 Mail::to($user->email)->send(new MailQuenMatKhau($user));
 
-    		 $user->mat_khau=Hash::make($user->mat_khau);
-    		 $user->save();
-    		 $res = [
-                'success'   => true,
-                'msg'       => 'Tìm mật khẩu thành công'
-            ];
-            return \response()->json($res);
-    	}
+        		 $user->mat_khau=Hash::make($user->mat_khau);
+        		 $user->save();
+        		 $res = [
+                    'success'   => true,
+                    'msg'       => 'Tìm mật khẩu thành công'
+                ];
+                return \response()->json($res);
+        	}
+        }
 
     }
 }
